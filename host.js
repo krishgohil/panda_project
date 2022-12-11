e61194b3cd26c296fe1144fd880fac55907ff0dd
Krish Gohil <user email>
2022-12-11 11:45:06 +0530

    change

--- host.js
+++ host.js
@@ -1,2 +1,2 @@
-export const host = "http://localhost:3000"
-// export const host = "https://ubout.vercel.app"
+// export const host = "http://localhost:3000"
+export const host = "https://ubout.vercel.app"
